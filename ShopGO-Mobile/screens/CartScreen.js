074539d8847b4c3023d73c16b6875c38908d3f8d
CaramelMisto <selekahmetcangs02@gmail.com>
2025-05-19 17:03:54 +0300

    ace

---
+++ ShopGO-Mobile/screens/CartScreen.js
@@ -0,0 +1,11 @@
+// screens/CartScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CartScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sepet Sayfası</Text>
+    </View>
+  );
+}
